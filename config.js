f52a6190649a94cfedbd42d72b96b3a6537f3802
Congvien099 <94543198+Congvien099@users.noreply.github.com>
2021-11-18 09:04:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    titleWeb: "the gioi cua toi",
-    introTitle: 'bạn yêu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "hello",
+    introTitle: 'bạn ',
+    introDesc: `bị điên hả`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    title: 'bạn bị điên đúng khônh',
+    desc: 'bạn bị điên phải không?',
+    btnYes: 'đúng rồi <33',
+    btnNo: 'Không nha:3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại điên<3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà😍. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'inb cho mình nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/vien.cong.100',
+    reply: 'mình bị điên <33333333',
+    mess: 'Mình biết mà',
+    messDesc: 'bạn bị điên thật hả.',
+    btnAccept: 'đúng rồi <3',
+    
